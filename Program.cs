98faf6e67a1c687aa41d24a767fbda8c018d19dc
barisdemirkap <barisdemirkap@gmail.com>
2021-12-31 16:07:11 +0300

    if else statements added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace if_else_if
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour ;
+            if(time >= 6 && time <= 11)
+                Console.WriteLine("Gunaydin!!") ;
+            else if(time <= 18)
+                Console.WriteLine("Iyi Gunler!!") ;
+            else 
+                Console.WriteLine("Iyi GECELER!!") ;
+
+            string result = time <=18 ? "Iyi Gunler!!" : "Iyi GECELER!!" ;
+            result = time >= 6 && time<11 ? "Gunaydin!!" : time <=18 ? "Iyi Gunler!!" : "Iyi Geceler" ;
+            Console.WriteLine(result) ;
+            
+           
+        }
+
+    }
+}
